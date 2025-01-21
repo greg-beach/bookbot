d34f91a0571df6d9120ac9354705ffd51e6d20c8
greg-beach <gbeach22@gmail.com>
2025-01-21 17:42:12 -0500

    Completed project that prints out report of document

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ def main():
     def sort_on(dict):
         return dict["value"]
 
+    words = file_contents.split()
+    word_count = len(words)
+
     character_dict = {}
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document\n")
 
     for letter in file_contents.lower(): 
         if letter not in alphabet:
@@ -22,6 +28,11 @@ def main():
         dict_list.append({'letter': letter, "value": value})
 
     dict_list.sort(reverse=True, key=sort_on)
+
+    for sorted_dict in dict_list:
+        print(f"The '{sorted_dict['letter']}' character was found {sorted_dict['value']} times")
+
+    print("--- End Report ---")
 
 
 
