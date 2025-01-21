f999f1ee985909046bf4889e9dc176b27c85a4ee
greg-beach <gbeach22@gmail.com>
2025-01-21 17:19:40 -0500

    modify main.py to take in a text and count the words

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    with open('books/frankenstein.txt') as f:
+        file_contents = f.read()
+    
+    words = file_contents.split()
+
+    print(len(words))
+
+if __name__ == "__main__":
+    main()
